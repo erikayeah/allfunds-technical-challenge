2515757bbb58abedcbe48294059766e45c6b42d0
ErikaFogar <erikafogar.j@gmail.com>
2025-08-08 00:17:39 +0200

    feat: export routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,10 @@
+import express from "express";
+const routes = express.Router();
+
 import getNews from "../controllers/getNews.js";
 import getNewByID from "../controllers/getNewById.js";
 import createNew from "../controllers/createNew.js";
 import updateNewByID from "../controllers/updateNewByID.js";
-
-import express from "express";
-const route = express.Router();
 
 route.get("/", getNews);
 
@@ -13,3 +13,5 @@ route.get("/:id", getNewByID);
 route.patch("/:id", updateNewByID);
 
 route.post("/", createNew);
+
+export default routes;
