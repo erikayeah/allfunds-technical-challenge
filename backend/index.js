5600f0ec26c0426128a47e352fcf8435c93e368e
ErikaFogar <erikafogar.j@gmail.com>
2025-08-08 14:21:45 +0200

    fix: add body/parser for MongoDB integration

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
-const express = require("express");
+import express from "express";
+import "dotenv/config";
+import routesNews from "./routes/routes.js";
+import bodyParser from "body-parser";
+
 const app = express();
-const port = 3000;
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/news", routesNews);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Allfunds!");
 });
 
-app.listen(port, () => {
-  console.log(`News app listening on port ${port}`);
-});
+try {
+  const port = process.env.port || 3000;
+  app.listen(port, () => {
+    console.log(`News app listening on port ${port}`);
+  });
+} catch (error) {
+  console.log(error);
+}
