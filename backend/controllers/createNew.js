5d6c024abefb6bbfcdcb71dd14c20052bdb725b1
ErikaFogar <erikafogar.j@gmail.com>
2025-08-08 14:12:26 +0200

    feat: add controller to get news by ID

--- backend/controllers/createNew.js
+++ backend/controllers/createNew.js
@@ -1,9 +1,12 @@
+import newsModel from "../models/newsModels.js";
+
 const createNew = async (req, res) => {
   try {
-    res.status(201).json("create ok");
+    const data = await newsModel.create(req.body);
+    res.status(201).json(data);
     // CONTINUE
   } catch (error) {
-    return res.status(500).send(error);
+    return res.status(500).json({ error: error.message });
   }
 };
 
