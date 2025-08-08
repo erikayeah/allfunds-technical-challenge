7a7b06ba1382535522754f30a5937dd8dad56253
ErikaFogar <erikafogar.j@gmail.com>
2025-08-08 14:08:43 +0200

    feat: add MongoDB client configuration

--- backend/config/dbClient.js
+++ backend/config/dbClient.js
@@ -1,16 +1,18 @@
 import { MongoClient } from "mongodb";
 import "dotenv/config";
 
+const { user_db, password_db, server_db, name_db } = process.env;
+
 class dbClient {
   constructor() {
-    const uri = `mongodb+srv://${process.env.user_db}:${process.env.password_db}@${process.env.server_db}/?retryWrites=true&w=majority&appName=allfundsTC`;
+    const uri = `mongodb+srv://${user_db}:${password_db}@${server_db}/?retryWrites=true&w=majority&appName=allfundsTC`;
     this.client = new MongoClient(uri);
     this.connectDB();
   }
   async connectDB() {
     try {
       await this.client.connect();
-      this.db = this.client.db(process.env.name_db);
+      this.db = this.client.db(name_db);
       console.log("Connected to MongoDB");
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
