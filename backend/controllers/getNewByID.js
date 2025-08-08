2cbeba9371759e6a9cd1b90dde824c496d872cb6
ErikaFogar <erikafogar.j@gmail.com>
2025-08-08 14:15:36 +0200

    feat: add controller to get new by id

--- backend/controllers/getNewByID.js
+++ backend/controllers/getNewByID.js
@@ -9,7 +9,6 @@ const getNewByID = async (req, res) => {
 
     if (!id) {
       return res.status(400).send("News ID is required");
-      // CONTINUE
     }
   } catch (error) {
     return res.status(500).json({ error: error.message });
