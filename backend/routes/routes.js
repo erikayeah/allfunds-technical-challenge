7b31af43fe2e0754a6fe84f6aad60d093452058e
ErikaFogar <erikafogar.j@gmail.com>
2025-08-08 14:19:36 +0200

    fix: correct function name i routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,13 +2,13 @@ import express from "express";
 const route = express.Router();
 
 import getNews from "../controllers/getNews.js";
-import getNewByID from "../controllers/getNewById.js";
+import getByID from "../controllers/getNewById.js";
 import createNew from "../controllers/createNew.js";
 import updateNewByID from "../controllers/updateNewByID.js";
 
 route.get("/", getNews);
 
-route.get("/:id", getNewByID);
+route.get("/:id", getByID);
 
 route.patch("/:id", updateNewByID);
 
