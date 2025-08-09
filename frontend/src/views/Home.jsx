070ea34e0f46a7a09f2d537fc5ec12e398b12e49
ErikaFogar <erikafogar.j@gmail.com>
2025-08-09 12:50:32 +0200

    feat: add Home and Archived views

---
+++ frontend/src/views/Home.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { setContext } from "svelte";
+import Loading from "../components/loading/Loading";
+import NavBar from "../components/navBar/NavBar";
+import axios from "axios";
+
+const Home = () => {
+  const API_URL = "http://localhost:5100";
+
+  const [loading, setLoading] = useState(true);
+  const [newsList, setNews] = useState([]);
+
+  const getNews = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/`);
+      console.log("Response data:", response.data);
+      setNews(response.data);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  useEffect(() => {
+    getNews();
+    console.log("News:", newsList);
+    setLoading(false);
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      {loading ? <Loading /> : <h1> ALL NEWS</h1>}
+    </div>
+  );
+};
+
+export default Home;
