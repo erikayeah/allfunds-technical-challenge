c5454efeb9712706dec449adb5ec17f7edf85612
ErikaFogar <erikafogar.j@gmail.com>
2025-08-09 19:15:16 +0200

    feat: show news list with sort, archive button and auto refresh

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,35 +1,61 @@
 import { useEffect, useState } from "react";
-import { setContext } from "svelte";
 import Loading from "../components/loading/Loading";
 import NavBar from "../components/navBar/NavBar";
+import NewsList from "../components/newsList/NewsList";
 import axios from "axios";
 
 const Home = () => {
   const API_URL = "http://localhost:5100/news";
 
   const [loading, setLoading] = useState(true);
-  const [newsList, setNews] = useState([]);
+  const [allNewsList, setAllNews] = useState([]);
 
-  const getNews = async () => {
+  const getAllNews = async () => {
     try {
       const response = await axios.get(`${API_URL}/`);
-      console.log("Response data:", response.data);
-      setNews(response.data);
+      const filteredNews = response.data.filter((news) => !news.archiveDate);
+
+      const sortedNews = filteredNews.sort(
+        (a, b) => new Date(b.date) - new Date(a.date)
+      );
+      setAllNews(sortedNews);
+      if (loading) {
+        setLoading(false);
+      }
     } catch (error) {
       console.error("Error", error);
     }
   };
 
+  const buttonArchive = async (id, date) => {
+    try {
+      await axios.patch(`${API_URL}/${id}`, {
+        archiveDate: new Date().toISOString(),
+      });
+      getAllNews();
+    } catch (error) {
+      console.error("Error archiving news", error);
+    }
+  };
+
   useEffect(() => {
-    getNews();
-    console.log("News:", newsList);
-    setLoading(false);
+    getAllNews();
+    const interval = setInterval(getAllNews, 10000);
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return (
     <div>
-      <NavBar />
-      {loading ? <Loading /> : <h1> ALL NEWS</h1>}
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <NavBar />
+          <NewsList allNewsList={allNewsList} buttonArchive={buttonArchive} />
+        </>
+      )}
     </div>
   );
 };
