0fba1dd0a5517248b779280b67aa70e5e6ffd3f4
ErikaFogar <erikafogar.j@gmail.com>
2025-08-09 19:08:04 +0200

    feat: add archive news view

---
+++ frontend/src/views/ArchivedNews.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import Loading from "../components/loading/Loading";
+import NavBar from "../components/navBar/NavBar";
+import NewsList from "../components/newsList/NewsList";
+import axios from "axios";
+
+const ArchivedNews = () => {
+  const API_URL = "http://localhost:5100/news";
+
+  const [loading, setLoading] = useState(true);
+  const [allNewsList, setAllNews] = useState([]);
+
+  const getAllNews = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/`);
+      const filteredNews = response.data.filter((news) => news.archiveDate);
+      const sortedNews = filteredNews.sort(
+        (a, b) => new Date(b.archiveDate) - new Date(a.archiveDate)
+      );
+      setAllNews(sortedNews);
+      if (loading) {
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  const buttonDelete = async (id, date) => {
+    try {
+      await axios.delete(`${API_URL}/${id}`);
+      getAllNews();
+    } catch (error) {
+      console.error("Error archiving news", error);
+    }
+  };
+
+  useEffect(() => {
+    getAllNews();
+    const interval = setInterval(getAllNews, 5000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <NavBar />
+          <NewsList allNewsList={allNewsList} buttonDelete={buttonDelete} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ArchivedNews;
