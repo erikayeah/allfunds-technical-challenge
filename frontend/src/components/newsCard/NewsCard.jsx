e410851eaf4aa1fb88cecb4106049d9b67b5b8fe
ErikaFogar <erikafogar.j@gmail.com>
2025-08-09 19:11:52 +0200

    feat: add NewsList and NewsCard component with rendering logic

--- frontend/src/components/newsCard/NewsCard.jsx
+++ frontend/src/components/newsCard/NewsCard.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 
-const NewsCard = () => {
+const NewsCard = ({ news, buttonArchive, buttonDelete }) => {
+  console.log("NewsCard props:", news);
+
+  const { title, description, date, content, author, archiveDate, _id } = news;
+
   return (
-    <div>
-      <h1>NEWS CARD </h1>
+    <div
+      style={{ border: "1px solid #ccc", padding: "10px", margin: "10px 0" }}
+    >
+      {!archiveDate && (
+        <button onClick={() => buttonArchive(_id)}>Archive</button>
+      )}
+      {archiveDate && <button onClick={() => buttonDelete(_id)}>Remove</button>}
+      <h2>{title}</h2>
+      <p>
+        <strong>Description:</strong> {description}
+      </p>
+
+      <p>{content}</p>
+
+      <p>
+        Publisded by {author} on {date}{" "}
+      </p>
+      {archiveDate && (
+        <p>Archived on {new Date(archiveDate).toLocaleDateString()}</p>
+      )}
     </div>
   );
 };
