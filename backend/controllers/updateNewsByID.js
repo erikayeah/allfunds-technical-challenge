ac59cd69adf23ad852e55c9a05c30005659abf0e
ErikaFogar <erikafogar.j@gmail.com>
2025-08-10 19:48:14 +0200

    chore: remove console.log

--- backend/controllers/updateNewsByID.js
+++ backend/controllers/updateNewsByID.js
@@ -8,7 +8,6 @@ const updateNewsByID = async (req, res) => {
       return res.status(400).send("News ID is required");
     }
     const data = await newsModel.updateByID(id, req.body);
-    console.log(`Updating news with ID: ${id}`);
 
     res.status(201).json(data);
   } catch (error) {
