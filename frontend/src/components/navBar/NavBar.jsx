403b028fbc3c6776faf5f12f4bf8086596f0be85
ErikaFogar <erikafogar.j@gmail.com>
2025-08-10 02:04:14 +0200

    feat: update components with Bootstrap elements

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -1,15 +1,33 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../../assets/allfundslogo.webp";
+import style from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
     <>
-      <Link to={"/"}>
-        <button>ALL NEWS</button>
-      </Link>
-      <Link to={"/archived"}>
-        <button>ARCHIVED NEWS</button>
-      </Link>
+      <Navbar expand="lg" className={style.container}>
+        <Container>
+          <Navbar.Brand>
+            <a href="https://allfunds.com/es/blog/" target="_blank">
+              <img src={logo} className={style.logo} />
+            </a>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                News
+              </Nav.Link>
+              <Nav.Link as={Link} to="/archived">
+                Archived News
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
