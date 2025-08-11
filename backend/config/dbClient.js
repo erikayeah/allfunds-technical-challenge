ea62d206f5230adf8e560461d9e70a02d7988e07
ErikaFogar <erikafogar.j@gmail.com>
2025-08-11 13:00:54 +0200

    feat: add schema with collMod method

--- backend/config/dbClient.js
+++ backend/config/dbClient.js
@@ -12,6 +12,49 @@ class dbClient {
     try {
       await this.client.connect();
       this.db = this.client.db(NAME_DB);
+      await this.db.runCommand({
+        collMod: "news",
+        validator: {
+          $jsonSchema: {
+            bsonType: "object",
+            title: "News Object Validation",
+            required: [
+              "_id",
+              "title",
+              "description",
+              "date",
+              "content",
+              "author",
+            ],
+            properties: {
+              _id: {
+                bsonType: "objectId",
+                description: "'_id' must be an ObjectId and is required",
+              },
+              title: {
+                bsonType: "string",
+                description: "'title' must be a string and is required",
+              },
+              description: {
+                bsonType: "string",
+                description: "'descriptionle' must be a string and is required",
+              },
+              date: {
+                bsonType: "date",
+                description: "'date' is required",
+              },
+              content: {
+                bsonType: "string",
+                description: "'content' must be a string and is required",
+              },
+              author: {
+                bsonType: "string",
+                description: "'author' must be a string and is required",
+              },
+            },
+          },
+        },
+      });
       console.log("Connected to MongoDB");
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
