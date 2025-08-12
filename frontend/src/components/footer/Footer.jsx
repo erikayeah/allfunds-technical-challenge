1c02d00c9ed0f4e9e4c36bea73676f05e6ca0aa0
ErikaFogar <erikafogar.j@gmail.com>
2025-08-12 02:03:36 +0200

    feat: add footer

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>© 2025 Erika Judith Fogar - All rights reserved</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
